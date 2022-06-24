ff0152b3d74cab15d4c88318f08e92e0be0db49f
Mirza <mirzaanas442@gmail.com>
2022-06-24 18:23:20 +0500

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 var tempBidPrice = 0.00;
+var bitrexBidPrice = 0.00;
+var bitsBidPrice = 0.00;
 function getUpdates() {
 	const ws = new WebSocket("ws://localhost:8080");
 	ws.addEventListener("open", () =>{
@@ -7,25 +9,62 @@ function getUpdates() {
 	});
 
 	ws.addEventListener('message', function (event) {
-	  var bid = JSON.parse(event.data);
-	  if(bid && bid.kraken){
-	  	//var bidData = JSON.parse(event.data).XBTUSD['b'];
-		var bidData = bid.kraken.b;
-		var bidPrice = bidData[0]; // BID PRICE
-		var bidVolume = bidData[2]; // VOLUME
-		
-		var ticker = $('.js-exchange-ticker');
-		if (bidPrice >= tempBidPrice) {
+	    var bid = JSON.parse(event.data);
+		if(bid && bid.kraken){
+			var bidData = bid.kraken.b;
+			var bidPrice = bidData[0]; // BID PRICE
+			var bidVolume = bidData[2]; // VOLUME
+			
+			var ticker = $('.js-exchange-ticker');
+			if (bidPrice >= tempBidPrice) {
+				ticker.removeClass('gg-arrow-down');
+				ticker.addClass('gg-arrow-up');
+			} else {
+				ticker.addClass('gg-arrow-down');
+				ticker.removeClass('gg-arrow-up');
+			}
+			tempBidPrice = bidPrice; // Reset Temp Bid Price
+			
+			$('.js-bid-price').text(tempBidPrice);
+			$('.js-bid-volume').text(bidVolume);
+		}
+
+		if(bid && bid.bittrex){
+			var bidData = bid.bittrex;
+			var bidPrice = bidData.bidRate; // BID PRICE
+			var bidVolume = bidData.askRate; // Ask Rate
+			
+			var ticker = $('.bittrex-js-exchange-ticker');
+			if (bidPrice >= bitrexBidPrice) {
 			ticker.removeClass('gg-arrow-down');
 			ticker.addClass('gg-arrow-up');
-		} else {
+			} else {
 			ticker.addClass('gg-arrow-down');
 			ticker.removeClass('gg-arrow-up');
+			}
+			bitrexBidPrice = bidPrice; // Reset Temp Bid Price
+			
+			$('.bit-bid-price').text(bitrexBidPrice);
+			$('.bit-bid-volume').text(bidVolume);
 		}
-		tempBidPrice = bidPrice; // Reset Temp Bid Price
-		
-		$('.js-bid-price').text(tempBidPrice);
-		$('.js-bid-volume').text(bidVolume);
-	   }
+
+		if(bid && bid.bitsmap){
+			var bidData = bid.bitsmap;
+			var bidPrice = bidData.price; // BID PRICE
+			var bidVolume = bidData.amount; // Amount
+			
+			var ticker = $('.bits-js-exchange-ticker');
+			if (bidPrice >= bitsBidPrice) {
+			ticker.removeClass('gg-arrow-down');
+			ticker.addClass('gg-arrow-up');
+			} else {
+			ticker.addClass('gg-arrow-down');
+			ticker.removeClass('gg-arrow-up');
+			}
+			bitsBidPrice = bidPrice; // Reset Temp Bid Price
+			
+			$('.bits-bid-price').text(bitsBidPrice);
+			$('.bits-bid-volume').text(bidVolume);
+		}
 	});
 }
